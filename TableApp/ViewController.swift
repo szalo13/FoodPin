2735bd545f2bf5fc9f3de1803a47fa8bd3da1a84
szalo13 <kamil_sza@yahoo.pl>
2017-04-09 23:27:13 +0200

    Adding simple restaurants object table and images

--- TableApp/ViewController.swift
+++ TableApp/ViewController.swift
@@ -10,13 +10,52 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po' Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    var restaurants:[Restaurant] = [
+        Restaurant(name: "Cafe Deadend", type: "Coffee & Tea Shop", location: "Hong Kong", image: "cafedeadend.jpg", isVisited: false),
+            Restaurant(name: "Homei", type: "Cafe", location: "Hong Kong", image:
+                "homei.jpg", isVisited: false),
+            Restaurant(name: "Teakha", type: "Tea House", location: "Hong Kong", image:
+                "teakha.jpg", isVisited: false),
+            Restaurant(name: "Cafe loisl", type: "Austrian / Causual Drink", location:
+                "Hong Kong", image: "cafeloisl.jpg", isVisited: false),
+            Restaurant(name: "Petite Oyster", type: "French", location: "Hong Kong",
+                       image: "petiteoyster.jpg", isVisited: false),
+            Restaurant(name: "For Kee Restaurant", type: "Bakery", location: "Hong Kong", image: "forkeerestaurant.jpg", isVisited: false),
+                Restaurant(name: "Po's Atelier", type: "Bakery", location: "Hong Kong",
+                           image: "posatelier.jpg", isVisited: false),
+                Restaurant(name: "Bourke Street Backery", type: "Chocolate", location:
+                    "Sydney", image: "bourkestreetbakery.jpg", isVisited: false),
+                Restaurant(name: "Haigh's Chocolate", type: "Cafe", location: "Sydney",
+                           image: "haighschocolate.jpg", isVisited: false),
+                Restaurant(name: "Palomino Espresso", type: "American / Seafood", location:
+                    "Sydney", image: "palominoespresso.jpg", isVisited: false),
+                Restaurant(name: "Upstate", type: "American", location: "New York", image:
+                    "upstate.jpg", isVisited: false),
+                Restaurant(name: "Traif", type: "American", location: "New York", image:
+                    "traif.jpg", isVisited: false),
+                Restaurant(name: "Graham Avenue Meats", type: "Breakfast & Brunch",
+                           location: "New York", image: "grahamavenuemeats.jpg", isVisited: false),
+                Restaurant(name: "Waffle & Wolf", type: "Coffee & Tea", location: "New York", image: "wafflewolf.jpg", isVisited: false),
+                    Restaurant(name: "Five Leaves", type: "Coffee & Tea", location: "New York",
+                               image: "fiveleaves.jpg", isVisited: false),
+                    Restaurant(name: "Cafe Lore", type: "Latin American", location: "New York",
+                               image: "cafelore.jpg", isVisited: false),
+                    Restaurant(name: "Confessional", type: "Spanish", location: "New York",
+                               image: "confessional.jpg", isVisited: false),
+                    Restaurant(name: "Barrafina", type: "Spanish", location: "London", image:
+                        "barrafina.jpg", isVisited: false),
+                    Restaurant(name: "Donostia", type: "Spanish", location: "London", image: "donostia.jpg", isVisited: false),
+                    Restaurant(name: "Royal Oak", type: "British", location: "London", image:
+                        "royaloak.jpg", isVisited: false),
+                    Restaurant(name: "CASK Pub and Kitchen", type: "Thai", location: "London",
+                               image: "caskpubkitchen.jpg", isVisited: false)
+    ]
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int ) ->
     Int {
         //Return numbers of rows in the section
-        return restaurantNames.count
+        return restaurants.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
@@ -25,8 +64,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
         
         //configure the cell
-        cell.textLabel?.text = restaurantNames[indexPath.row]
-        cell.imageView?.image = UIImage(named: "restaurant")
+        cell.textLabel?.text = restaurants[indexPath.row].name
+        cell.imageView?.image = UIImage(named: restaurants[indexPath.row].image)
             
         return cell
     }
