3481239178cf8cbed9b1b000653fed077624936d
szalo13 <kamil_sza@yahoo.pl>
2017-04-10 12:14:36 +0200

    Creating custom table cell

---
+++ TableApp/RestaurantTableViewController.swift
@@ -0,0 +1,144 @@
+//
+//  RestaurantTableViewController.swift
+//  FoodPin
+//
+//  Created by Kamil Szalek on 09/04/2017.
+//  Copyright © 2017 Kamil Szalek. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewController: UITableViewController {
+
+    var restaurants:[Restaurant] = [
+        Restaurant(name: "Cafe Deadend", type: "Coffee & Tea Shop", location: "Hong Kong", image: "cafedeadend.jpg", isVisited: false),
+            Restaurant(name: "Homei", type: "Cafe", location: "Hong Kong", image:
+                "homei.jpg", isVisited: false),
+            Restaurant(name: "Teakha", type: "Tea House", location: "Hong Kong", image:
+                "teakha.jpg", isVisited: false),
+            Restaurant(name: "Cafe loisl", type: "Austrian / Causual Drink", location:
+                "Hong Kong", image: "cafeloisl.jpg", isVisited: false),
+            Restaurant(name: "Petite Oyster", type: "French", location: "Hong Kong",
+                       image: "petiteoyster.jpg", isVisited: false),
+            Restaurant(name: "For Kee Restaurant", type: "Bakery", location: "Hong Kong", image: "forkeerestaurant.jpg", isVisited: false),
+                Restaurant(name: "Po's Atelier", type: "Bakery", location: "Hong Kong",
+                           image: "posatelier.jpg", isVisited: false),
+                Restaurant(name: "Bourke Street Backery", type: "Chocolate", location:
+                    "Sydney", image: "bourkestreetbakery.jpg", isVisited: false),
+                Restaurant(name: "Haigh's Chocolate", type: "Cafe", location: "Sydney",
+                           image: "haighschocolate.jpg", isVisited: false),
+                Restaurant(name: "Palomino Espresso", type: "American / Seafood", location:
+                    "Sydney", image: "palominoespresso.jpg", isVisited: false),
+                Restaurant(name: "Upstate", type: "American", location: "New York", image:
+                    "upstate.jpg", isVisited: false),
+                Restaurant(name: "Traif", type: "American", location: "New York", image:
+                    "traif.jpg", isVisited: false),
+                Restaurant(name: "Graham Avenue Meats", type: "Breakfast & Brunch",
+                           location: "New York", image: "grahamavenuemeats.jpg", isVisited: false),
+                Restaurant(name: "Waffle & Wolf", type: "Coffee & Tea", location: "New York", image: "wafflewolf.jpg", isVisited: false),
+                    Restaurant(name: "Five Leaves", type: "Coffee & Tea", location: "New York",
+                               image: "fiveleaves.jpg", isVisited: false),
+                    Restaurant(name: "Cafe Lore", type: "Latin American", location: "New York",
+                               image: "cafelore.jpg", isVisited: false),
+                    Restaurant(name: "Confessional", type: "Spanish", location: "New York",
+                               image: "confessional.jpg", isVisited: false),
+                    Restaurant(name: "Barrafina", type: "Spanish", location: "London", image:
+                        "barrafina.jpg", isVisited: false)
+        ]
+                    
+                    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return restaurants.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cellIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! RestaurantTableViewCell
+        
+        // Configure the cell
+        cell.nameLabel.text = restaurants[indexPath.row].name
+        cell.thumbnailImageView.image = UIImage(named: restaurants[indexPath.row].image)
+        
+        return cell
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
