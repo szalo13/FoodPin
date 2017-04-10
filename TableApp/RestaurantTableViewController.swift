d22e157cc2b3e4173f91c4a7e1a3560c861e7631
szalo13 <kamil_sza@yahoo.pl>
2017-04-10 14:06:20 +0200

    Adding delete swipe button

--- TableApp/RestaurantTableViewController.swift
+++ TableApp/RestaurantTableViewController.swift
@@ -170,17 +170,17 @@ class RestaurantTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
         if editingStyle == .delete {
-            // Delete the row from the data source
+            restaurants.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
+        
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
